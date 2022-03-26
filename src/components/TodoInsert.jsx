b786bcf2a84b8f6ca51d656af86f72c73bc28fa7
ella <ella@choeja-eun-ui-MacBookAir.local>
2022-03-26 17:17:52 +0900

    update onSubmit function

--- src/components/TodoInsert.jsx
+++ src/components/TodoInsert.jsx
@@ -1,7 +1,22 @@
-const TodoInsert = () => {
+import { useState } from "react";
+
+const TodoInsert = ({onInsertToggle, insertTodo}) => {
+    const [text, setText] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        insertTodo(text);
+        setText('');
+        onInsertToggle();
+    }
+
+    const onChange = (e) => {
+        setText(e.target.value);
+    }
+
     return (
-        <form>
-            <input />
+        <form onSubmit={onSubmit}>
+            <input value={text} onChange={onChange} />
             <button type='submit'>추가</button>
         </form>
     );
