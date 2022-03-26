10818b2a49713dfad5592ef6ebc12859a6e28bb5
ella <ella@choeja-eun-ui-MacBookAir.local>
2022-03-26 16:02:01 +0900

    made todos state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import Home from './components/Home';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "계획 1",
+      checked: false
+    },
+    {
+      id: 2,
+      text: "계획 2",
+      checked: true
+    },
+    {
+      id: 3,
+      text: "계획 3",
+      checked: false
+    }
+  ]);
   return (
     <Home>
       {/* <TodoList /> */}
